0663cdf49b12393ff50662d9aaa68317c34b4a3e
MarkMoorefield <155267237+MarkMoorefield@users.noreply.github.com>
2024-01-20 02:54:16 +0000

    Added the logic.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
+// Created a function called alphabetPosition.
+// Created a variable called alphabet, and plugged in all the letters. 
+// made the text variable lowercase and split it up into an array. 
+// Then I made it so that depending on the letter switched it to the corresponding number.
+// then I joined the numbers together into an array and returned it. 
+
+
 function alphabetPosition(text) {
-    var alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
     return text.toLowerCase().split('')
     .filter( letter => {
       let index = alphabet.indexOf(letter);
